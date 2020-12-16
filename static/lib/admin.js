b7c9cb8eba8aa25f2122c60fb7dbc347a377ae63
Julian Lam <julian@nodebb.org>
2020-12-16 12:27:02 -0500

    chore: remove additional ACP code I don't use

--- static/lib/admin.js
+++ static/lib/admin.js
@@ -1,13 +1,11 @@
 'use strict';
 
-/* globals $, app, socket, define, config */
+/* globals $, app, socket, define */
 
-define('admin/plugins/quickstart', ['settings', 'uploader', 'admin/modules/colorpicker'], function (settings, uploader, colorpicker) {
+define('admin/plugins/quickstart', ['settings', 'uploader', 'admin/modules/colorpicker'], function (settings) {
 	var ACP = {};
 
 	ACP.init = function () {
-		setupColorInputs();
-		setupUploader();
 		settings.load('quickstart', $('.quickstart-settings'));
 		$('#save').on('click', saveSettings);
 	};
@@ -22,37 +20,6 @@ define('admin/plugins/quickstart', ['settings', 'uploader', 'admin/modules/color
 				clickfn: function () {
 					socket.emit('admin.reload');
 				},
-			});
-		});
-	}
-
-	function setupColorInputs() {
-		var colorInputs = $('[data-settings="colorpicker"]');
-		colorpicker.enable(colorInputs, updateColors);
-		colorInputs.on('change', updateColors);
-		updateColors();
-	}
-
-	function updateColors() {
-		$('#preview').css({
-			color: $('#color').val(),
-			'background-color': $('#bgColor').val(),
-		});
-	}
-
-	function setupUploader() {
-		$('#content input[data-action="upload"]').each(function () {
-			var uploadBtn = $(this);
-			uploadBtn.on('click', function () {
-				uploader.show({
-					route: config.relative_path + '/api/admin/upload/file',
-					params: {
-						folder: 'quickstart',
-					},
-					accept: 'image/*',
-				}, function (image) {
-					$('#' + uploadBtn.attr('data-target')).val(image);
-				});
 			});
 		});
 	}
